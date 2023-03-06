b3b5fae177f5a30400e10fe04ed55e78e7bc763c
Aditya Wasankar <121103358+adityawasankar1@users.noreply.github.com>
2023-03-06 12:30:08 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -57,7 +57,7 @@ Return your list ordered alphabetically by email starting with A. */
 SELECT DISTINCT email,first_name, last_name
 FROM customer
 JOIN invoice ON customer.customer_id = invoice.customer_id
-JOIN invoiceline ON invoice.invoice_id = invoiceline.invoice_id
+JOIN invoice_line ON invoice.invoice_id = invoiceline.invoice_id
 WHERE track_id IN(
 	SELECT track_id FROM track
 	JOIN genre ON track.genre_id = genre.genre_id
